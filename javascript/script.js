2af99796d5a98ee30ab32c078d89aa2a9e6a28f8
Amanda Kuiack <akuiack@my.bcit.ca>
2021-03-30 15:33:58 -0700

    start over

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+let courseList = [
+    { code: 'ACIT 1515', course: 'Scripting for IT' },
+    { code: 'ACIT 1620', course: 'Fundamental Web Technologies' },
+    { code: 'MATH 1310', course: 'Technical Math for IT' },
+];
+
+let userInput;
